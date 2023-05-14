536b39db76bb2c192d3c531deb3526a341308392
Jorge Marin <jm.dev97@gmail.com>
2023-05-14 16:29:41 -0500

    Define proveedores de servicios

--- src/data/dataArrays.jsx
+++ src/data/dataArrays.jsx
@@ -234,3 +234,22 @@ export const places = [
       '-- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum sapiente accusantium hic, doloribus repellat! Accusamus illum quas asperiores dolor eius alias, fuga ab quasi accusantium consectetur sed, beatae consequuntur.\n\n -- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsum sapiente accusantium hic, doloribus repellat! Accusamus illum quas asperiores dolor eius alias, fuga ab quasi accusantium consectetur sed, beatae consequuntur.'
   },
 ];
+
+export const providers = [
+  {
+    providerId: 0,
+    name: 'Cruz del Sur',
+    photo_url: 'https://images.ctfassets.net/1nvpgv2kdfc0/1kqJ26hOIgs5ltRX21DURK/8764ce9a5c4f943c1006795ea2dac0bf/Cruz_del_Sur_bus.png'
+  },
+  {
+    providerId: 1,
+    name: 'LATAM Perú',
+    photo_url:
+      'https://elcomercio.pe/resizer/0WL_qDGXSvtMc2ecM2xy62ZSEbM=/1200x800/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/F33LKLQJBRHQREI2QPZWM3M6NQ.jpg'
+  },
+  {
+    providerId: 2,
+    name: 'Sonesta Hotel',
+    photo_url: 'https://media-cdn.tripadvisor.com/media/photo-s/1d/1e/58/1c/exterior.jpg'
+  }
+];
