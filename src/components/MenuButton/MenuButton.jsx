48c6bd90c9ab9f5d263010d4647135114d24c801
Jorge Marin <jm.dev97@gmail.com>
2023-05-14 06:14:46 -0500

    Maqueta estructura de MenuButton

--- src/components/MenuButton/MenuButton.jsx
+++ src/components/MenuButton/MenuButton.jsx
@@ -3,6 +3,21 @@ import { TouchableHighlight, Image, Text, View } from "react-native";
 import PropTypes from "prop-types";
 import styles from "./styles";
 
-export default function MenuButton() {
-  return;
+export default function MenuButton(props) {
+  const { title, onPress, source } = props;
+
+  return (
+    <TouchableHighlight onPress={onPress} underlayColor="rgba(128, 128, 128, 0.1)">
+      <View>
+        <Image source={source} />
+        <Text>{title}</Text>
+      </View>
+    </TouchableHighlight>
+  );
 }
+
+MenuButton.propTypes = {
+  onPress: PropTypes.func,
+  source: PropTypes.number,
+  title: PropTypes.string,
+};
