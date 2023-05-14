b48db70348a18783ec77975001c85665bf53813f
Jorge Marin <jm.dev97@gmail.com>
2023-05-14 08:54:38 -0500

    Define estructura de componente

---
+++ src/components/BackButton/BackButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TouchableHighlight, Image, } from "react-native";
+import PropTypes from "prop-types";
+import styles from "./styles";
+
+export default function BackButton(props) {
+  return;
+}
