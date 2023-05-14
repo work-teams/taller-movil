7d77b82c9f46e74080aa1a9cd8e0838ea9f6ecc2
Jorge Marin <jm.dev97@gmail.com>
2023-05-14 17:01:42 -0500

    Define funciones de categoría

---
+++ src/data/MockDataAPI.jsx
@@ -0,0 +1,24 @@
+import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { places, categories, providers } from './dataArrays';
+
+// interactuar con categoria
+export function getCategoryName(categoryId) {
+  let name;
+  categories.map(data => {
+    if (data.id == categoryId) {
+      name = data.name;
+    }
+  });
+  return name;
+}
+
+export function getCategoryById(categoryId) {
+  let category;
+  categories.map(data => {
+    if (data.id == categoryId) {
+      category = data;
+    }
+  });
+  return category;
+}
