1b05ac6bd746315788a201ae6cbbc758958eb944
Jorge Marin <jm.dev97@gmail.com>
2023-05-14 14:11:33 -0500

    Define categorias

---
+++ src/data/dataArrays.jsx
@@ -0,0 +1,20 @@
+export const categories = [
+  {
+    id: 0,
+    name: 'Costa',
+    photo_url:
+      'https://img.womondoo.com/insecure/size:1920:::/plain/https://media.womondoo.com/media/images/Playa_Roja.max-1350x1080.format-webp-lossless_bpDjGxK.webp'
+  },
+  {
+    id: 1,
+    name: 'Sierra',
+    photo_url:
+      'https://img.womondoo.com/insecure/size:1920:::/plain/https://media.womondoo.com/media/images/Interior_de_Machu_Pi.max-1350x1080.format-webp-lossless_ttOfxX6.webp'
+  },
+  {
+    id: 2,
+    name: 'Selva',
+    photo_url:
+      'https://elperuano.pe/fotografia/thumbnail/2021/11/07/000138274M.jpg'
+  }
+];
