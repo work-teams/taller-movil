6f1da0a6b7cd1bc1768cccb8c880eae1365c7b1e
Jorge Marin <jm.dev97@gmail.com>
2023-05-14 21:02:34 -0500

    Define estructura de componente

---
+++ src/components/ViewProvidersButton/ViewProvidersButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TouchableHighlight, Image, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+
+export default function ViewProvidersButton (props) {
+    return;
+}
