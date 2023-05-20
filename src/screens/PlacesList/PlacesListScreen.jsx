14adc0f49d261b12945ef68680f28b8daab534ca
Jorge Marin <jm.dev97@gmail.com>
2023-05-20 06:18:20 -0500

    Define vista PlacesList

--- src/screens/PlacesList/PlacesListScreen.jsx
+++ src/screens/PlacesList/PlacesListScreen.jsx
@@ -1,6 +1,38 @@
+import React, { useLayoutEffect } from "react";
+import { FlatList, Text, View, TouchableHighlight, Image } from "react-native";
 import styles from "./styles";
+import { getPlaces, getCategoryName } from "../../data/MockDataAPI";
 
 export default function PlacesListScreen(props) {
+  const { navigation, route } = props;
 
-  return;
+  const item = route?.params?.category;
+  const placesArray = getPlaces(item.id);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.title,
+      headerRight: () => <View />,
+    });
+  }, []);
+
+  const onPressPlace = (item) => {
+    navigation.navigate("Place", { item });
+  };
+
+  const renderPlaces = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressPlace(item)}>
+      <View style={styles.container}>
+        <Image style={styles.photo} source={{ uri: item.photo_url }} />
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.category}>{getCategoryName(item.categoryId)}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  return (
+    <View>
+      <FlatList vertical showsVerticalScrollIndicator={false} numColumns={2} data={placesArray} renderItem={renderPlaces} keyExtractor={(item) => `${item.placeId}`} />
+    </View>
+  );
 }
