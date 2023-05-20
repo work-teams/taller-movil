79e1254edfca88e3f2af730b3f90041f4427473b
Wiston Pastor <wiston.pastor@unmsm.edu.pe>
2023-05-20 08:21:07 -0500

    implementacion de home-styles

---
+++ src/screens/Home/HomeScreen.jsx
@@ -0,0 +1,43 @@
+import React, { useLayoutEffect } from "react";
+import { FlatList, Text, View, TouchableHighlight, Image } from "react-native";
+import styles from "./styles";
+import { places } from "../../data/dataArrays";
+import MenuImage from "../../components/MenuImage/MenuImage";
+import { getCategoryName } from "../../data/MockDataAPI";
+
+export default function HomeScreen(props) {
+  const { navigation } = props;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <MenuImage
+          onPress={() => {
+            navigation.openDrawer();
+          }}
+        />
+      ),
+      headerRight: () => <View />,
+    });
+  }, []);
+
+  const onPressPlace = (item) => {
+    navigation.navigate("Place", { item });
+  };
+
+  const renderPlaces = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressPlace(item)}>
+      <View style={styles.container}>
+        <Image style={styles.photo} source={{ uri: item.photo_url }} />
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.category}>{getCategoryName(item.categoryId)}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  return (
+    <View>
+      <FlatList vertical showsVerticalScrollIndicator={false} numColumns={2} data={places} renderItem={renderPlaces} keyExtractor={(item) => `${item.placeId}`} />
+    </View>
+  );
+}
