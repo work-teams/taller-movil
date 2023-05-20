c6caf981dc32abe0d13a52d3b3d334042330c912
Jorge Marin <jm.dev97@gmail.com>
2023-05-20 06:39:29 -0500

    Define vista ProvidersDetails

--- src/screens/ProvidersDetails/ProvidersDetailsScreen.jsx
+++ src/screens/ProvidersDetails/ProvidersDetailsScreen.jsx
@@ -1,6 +1,42 @@
+import React, { useLayoutEffect } from "react";
+import { FlatList, Text, View, Image, TouchableHighlight } from "react-native";
 import styles from "./styles";
+import { getProviderName, getAllProviders } from "../../data/MockDataAPI";
 
 export default function ProvidersDetailsScreen(props) {
+  const { navigation, route } = props;
 
-  return;
+  const item = route.params?.providers;
+  const providersArray = getAllProviders(item);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.title,
+      headerTitleStyle: {
+        fontSize: 16,
+      },
+    });
+  }, []);
+
+  const onPressProvider = (item) => {
+    let name = getProviderName(item.providerId);
+    let provider = item.providerId;
+    navigation.navigate("Provider", { provider, name });
+  };
+
+  const renderProvider = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressProvider(item[0])}>
+      <View style={styles.container}>
+        <Image style={styles.photo} source={{ uri: item[0].photo_url }} />
+        <Text style={styles.title}>{item[0].name}</Text>
+        <Text style={{ color: "grey" }}>{item[1]}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
+  return (
+    <View>
+      <FlatList vertical showsVerticalScrollIndicator={false} numColumns={3} data={providersArray} renderItem={renderProvider} keyExtractor={(item) => `${item.placeId}`} />
+    </View>
+  );
 }
--- src/screens/ProvidersDetails/styles.jsx
+++ src/screens/ProvidersDetails/styles.jsx
@@ -1,6 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
+const SCREEN_WIDTH = width < height ? width : height;
+
+const numColumns = 3;
+const PLACE_ITEM_HEIGHT = 100;
+const PLACE_ITEM_OFFSET = 10;
+const PLACE_ITEM_MARGIN = PLACE_ITEM_OFFSET * 2;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    margin: PLACE_ITEM_OFFSET,
+    marginTop: 30,
+    width: (SCREEN_WIDTH - PLACE_ITEM_MARGIN) / numColumns - PLACE_ITEM_OFFSET,
+    height: PLACE_ITEM_HEIGHT + 60
+  },
+  title: {
+    margin: 10,
+    marginBottom: 5,
+    color: 'black',
+    fontSize: 13,
+    textAlign: 'center'
+  },
+  photo: {
+    width: (SCREEN_WIDTH - PLACE_ITEM_MARGIN) / numColumns - PLACE_ITEM_OFFSET,
+    height: PLACE_ITEM_HEIGHT,
+    borderRadius: 60
+  }
 });
 
 export default styles;
