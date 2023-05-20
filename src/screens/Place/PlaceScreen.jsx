97db74270f9d741e987eb0f03484105e96cef2d0
Jorge Marin <jm.dev97@gmail.com>
2023-05-20 04:43:28 -0500

    Define estructura de archivos

---
+++ src/screens/Place/PlaceScreen.jsx
@@ -0,0 +1,7 @@
+import React, { useLayoutEffect, useRef, useState } from "react";
+import styles from "./styles";
+
+export default function PlaceScreen(props) {
+
+  return;
+}
---
+++ src/screens/Place/styles.jsx
@@ -0,0 +1,6 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const styles = StyleSheet.create({
+});
+
+export default styles;
