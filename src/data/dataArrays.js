ffd9339efc3037c825898f4f3ef52ac24518984f
ochperalta <oscar.chumpitaz2@unmsm.edu.pe>
2023-06-23 15:35:18 -0500

    implementacion de servicio y detalles de servicios

--- src/data/dataArrays.js
+++ src/data/dataArrays.js
@@ -31,9 +31,9 @@ export const places = [
     ],
     location: 'Lima',
     services: [
-      [0, 'Buses', 'Empresa de transporte interprovincial'],
-      [1, 'Aerolíneas', 'Empresa de transporte interprovincial'],
-      [2, 'Hoteles', 'Empresa de transporte interprovincial'],
+      [0, 'Buses'],
+      [1, 'Aerolíneas'],
+      [2, 'Hoteles'],
     ],
     description:
       '-- La Ciudad Sagrada de Caral representa el origen de la cultura andina y es la civilización más antigua de América, con más de 5.000 años de historia donde la UNESCO la declaró como Patrimonio Cultural de la Humanidad, el complejo está formado por varias construcciones, entre las que resaltan seis pirámides y sus plazas circulares. Por los instrumentos y restos encontrados se conoce que sus habitantes se dedicaron principalmente a la pesca y agricultura.\n\n -- Se ubica a 206 km al noroeste de Lima (4 horas en auto aproximadamente).'
