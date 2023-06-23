ac4cd76b9f2bb4f9324f18af10b704e40ce77822
ochperalta <oscar.chumpitaz2@unmsm.edu.pe>
2023-06-23 16:43:36 -0500

    services detail test

--- test/getAllServices/getAllServices.test.js
+++ test/getAllServices/getAllServices.test.js
@@ -11,8 +11,8 @@ describe('getAllServices', () => {
     it('should return an array of services with their corresponding IDs when given a valid array of IDs', () => {
         const idArray = [[0, 'id1'], [1, 'id2']];
         const expectedServices = [
-            [{ serviceId: 0, name: 'Cruz del Sur', photo_url: 'https://images.ctfassets.net/1nvpgv2kdfc0/1kqJ26hOIgs5ltRX21DURK/8764ce9a5c4f943c1006795ea2dac0bf/Cruz_del_Sur_bus.png' }, 'id1'],
-            [{ serviceId: 1, name: 'LATAM Perú', photo_url: 'https://elcomercio.pe/resizer/0WL_qDGXSvtMc2ecM2xy62ZSEbM=/1200x800/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/F33LKLQJBRHQREI2QPZWM3M6NQ.jpg' }, 'id2']
+            [{ serviceId: 0, name: 'Cruz del Sur', photo_url: 'https://images.ctfassets.net/1nvpgv2kdfc0/1kqJ26hOIgs5ltRX21DURK/8764ce9a5c4f943c1006795ea2dac0bf/Cruz_del_Sur_bus.png', description: 'Empresa de transporte terrestre interprovinsional' }, 'id1'],
+            [{ serviceId: 1, name: 'LATAM Perú', photo_url: 'https://elcomercio.pe/resizer/0WL_qDGXSvtMc2ecM2xy62ZSEbM=/1200x800/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/F33LKLQJBRHQREI2QPZWM3M6NQ.jpg', description: 'Aerolínea a nivel nacional' }, 'id2']
         ];
 
         const result = getAllServices(idArray);
