c9e11366ea1725c65b6aae9e974f6687aa732824
Wiston Pastor <wiston.pastor@unmsm.edu.pe>
2023-07-14 19:01:20 -0500

    test mock para devolver el mombre del servicio cuando exista en la base de datos

---
+++ test/getCategoryName/getCategoryNamem.test.js
@@ -0,0 +1,40 @@
+import {getCategoryName} from '../../src/data/handleFirebase';
+import { firebase } from '../../src/data/firebase';
+
+jest.mock('../../src/data/firebase', () => {
+    const firebase = jest.requireActual('../../src/data/firebase');
+    return {
+        firebase: {
+            firestore: jest.fn(() => ({
+                collection: jest.fn(() => ({
+                    doc: jest.fn((id) => ({
+                        get: jest.fn(() => {
+                            if (id === 0) { // Caso donde se obtienen los datos correctos de un servicio existente
+                                return Promise.resolve({
+                                    exists: true,
+                                    data: jest.fn(() => ({
+                                        name: 'Costa'
+                                    }))
+                                });
+                            } else { // Caso donde se obtiene null para un servicio inexistente
+                                return Promise.resolve({
+                                    exists: false,
+                                    data: jest.fn()
+                                });
+                            }
+                        })
+                    }))
+                }))
+            }))
+        }
+    };
+});
+
+//test para devolver el nombre del servicio cuando existe en la base de datos
+describe('getCategoryName', () => {
+    it('debe devolver el nombre del servicio cuando existe en la base de datos', async () => {
+        const categoryName = await getCategoryName(0);
+        expect(categoryName).toBe('Costa');
+    });
+    
+});
